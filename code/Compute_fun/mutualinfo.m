9a81e954956df4e87d049b77dffc30a905154271
JianHong <792585905@qq.com>
2021-11-09 10:31:08 +0800

    改进后的互信息量计算

--- code/Compute_fun/mutualinfo.m
+++ code/Compute_fun/mutualinfo.m
@@ -8,12 +8,14 @@ for i = 1:length(X)
     p_y_x = @(x,u) (1/sqrt(2*pi)*exp(-(x'-u).^2./2));
     p_y = @(x) p_y_x(x,delta*X)*(px');
 
-    p_yxi =@(x) min(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0) + max(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0);
-%min() + max() 的原因是去掉NaN，同时保留正负值
+    p_yxi =@(x) min(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0) ...
+              + max(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0);
+%min() + max() 的原因是当值过小时会产生NaN,该方法可以去掉NaN的同时保留正负值
 
 %     I = I + integral(p_yxi,-inf,inf,'ArrayValued',true);
-    I = I + integral(p_yxi,-abs(delta*X(i)*10),abs(delta*X(i)*10),'ArrayValued',true);
+    I = I + integral(p_yxi,delta*X(i)-abs(10*delta*X(i)),delta*X(i)+abs(10*delta*X(i)),'ArrayValued',true);
 end
+
 end
 
 
