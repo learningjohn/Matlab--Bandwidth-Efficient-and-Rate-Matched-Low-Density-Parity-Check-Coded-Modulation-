b69f36f9476c6a2b818dc06e691b4f734c36abdf
JianHong <792585905@qq.com>
2021-11-13 18:27:03 +0800

    理论QAM误符号率计算函数，封装

--- code/QAM_theorySer.m
+++ code/QAM_theorySer.m
@@ -1,5 +1,5 @@
 function [Ser] = QAM_theorySer(P,EsN0_dB,M)
-%计算QAM理论误符号率
+%计算理论QAM误符号率，适用条件M>=16
 %输入变量解释
 %P: QAM信号各个星座点的概率，矩阵大小应与QAM星座图对应
 %EsN0_dB： Es/N0的dB形式
@@ -12,11 +12,12 @@ function [Ser] = QAM_theorySer(P,EsN0_dB,M)
     QAM_dmat = (repmat([-sqrt(M)+1:2:sqrt(M)-1],sqrt(M),1)./2).^2 + (repmat([sqrt(M)-1:-2:-sqrt(M)+1]',1,sqrt(M))./2).^2;
     %计算M-QAM在当前概率P下的取值
     A =sum(sum( QAM_dmat.*P));
-    ErrA = erfc(sqrt(EsN0(i)/(4*A)))-(0.5*erfc(sqrt(EsN0(i)/(4*A))))^2;
-    ErrB = (3/2)*erfc(sqrt(EsN0(i)/(4*A)))-2*(0.5*erfc(sqrt(EsN0(i)/(4*A))))^2;
-    ErrC = 2*erfc(sqrt(EsN0(i)/(4*A)))-4*(0.5*erfc(sqrt(EsN0(i)/(4*A))))^2;
+    %三个点的
+    ErrA = erfc(sqrt(EsN0/(4*A)))-(0.5*erfc(sqrt(EsN0/(4*A))))^2;
+    ErrB = (3/2)*erfc(sqrt(EsN0/(4*A)))-2*(0.5*erfc(sqrt(EsN0/(4*A))))^2;
+    ErrC = 2*erfc(sqrt(EsN0/(4*A)))-4*(0.5*erfc(sqrt(EsN0/(4*A))))^2;
     
     matrix_err = [[ErrA;ones(sqrt(M)-2,1)*ErrB;ErrA],repmat([ErrB;ones(sqrt(M)-2,1)*ErrC;ErrB],1,sqrt(M)-2),[ErrA;ones(sqrt(M)-2,1)*ErrB;ErrA]];
-    SER= sum(sum(matrix_err.*P));
+    Ser= sum(sum(matrix_err.*P));
 end
 
