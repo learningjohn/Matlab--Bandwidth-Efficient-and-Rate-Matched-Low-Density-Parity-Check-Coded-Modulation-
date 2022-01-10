febc61fffcdd339aee2b4629b88a3595d1a20144
JianHong <792585905@qq.com>
2022-01-10 18:14:17 +0800

    找到了解交织中的部分问题

--- code/ASK_LDPC.m
+++ code/ASK_LDPC.m
@@ -1,35 +1,31 @@
 %ASK误码率测试
 clc;clear;
 addpath("ccdm\");addpath("Compute_fun\");addpath("mat_data\");
-load(R_SNR.mat);
+load('R_SNR.mat');
 %调制参数
-M_ASK = 16;
+M = 16;
 n = log2(M);
-R = (n-1)/n;
-R1 = n/(n+1);
+R = 3/4;
 
 nSim = 10000;
 %信道和编码相关的参数
-snr = 20:0.1:20.5;     %信噪比
-PX = initialize_PX(snr,M_ASK);
+snr = 20.3:0.05:20.5;     %信噪比
+PX = initialize_PX(snr,M);
 EsN0=10.^(snr/10);
 LDPC_bitlength = 64800; %码长可在648，1296   1944，64800中选择
 n_ASK = LDPC_bitlength/n;  %ASK信号的长度
 
 if LDPC_bitlength == 64800 %如果LDPC编码长度为64800则使用DVB-S2标准
     H = dvbs2ldpc(R);            %LDPC的H矩阵
-    H1 = dvbs2ldpc(R1);
+   
 else                       %如果长度为其他则使用ieee标准
     H =sparse(buildH( LDPC_bitlength, R));            %LDPC的H矩阵
-    H1 =sparse(buildH( LDPC_bitlength, R1));
+   
 end
 %初始化固定码率的LDPC矩阵
 ldpcEncoder = comm.LDPCEncoder(H);
 ldpcDecoder = comm.LDPCDecoder(H);
 
-%初始化其他码率的矩阵
-ldpcEncoder1 = comm.LDPCEncoder(H1);
-ldpcDecoder1 = comm.LDPCDecoder(H1);
 
 %ASK正值信号映射表
 ASK_map = 1:2:2^(n)-1;
@@ -67,11 +63,11 @@ for ii = 1:length(snr)
 
     nerr_bit = 0;
     nerr = 0;
-    parfor k = 1:nSim
+    for k = 1:nSim
         %信源比特
         txBits_I_Uniform = randi(2,1,R*LDPC_bitlength)'-1;
         %不进行概率成型
-        encData_I_bit_Uniform = reshape(ldpcEncoder(txBits_I_Uniform),n/2,[])';
+        encData_I_bit_Uniform = reshape(ldpcEncoder(txBits_I_Uniform),n,[])';
         %交织
         pimap_Tx_I_Bit_Uniform = encData_I_bit_Uniform(:,bit_map_pi_Uniform);
         %Gray映射
@@ -83,20 +79,20 @@ for ii = 1:length(snr)
         Standard_variance1_Uniform=sqrt(variance_Uniform);
         noise=randn(1,n_ASK).';
         %AWGN
-        Rx_QAM_Uniform = out_Tx_I_Uniform.' + Standard_variance1_Uniform.*noise;
+        Rx_I_Uniform = out_Tx_I_Uniform.' + Standard_variance1_Uniform.*noise;
         
         %计算格雷映射下每一个位的对数似然比
         llr_I_Uniform = zeros(n_ASK,n);
         for j = 1:n
             %I路各比特位LLR
             %随机
-            llr_I_Uniform(:,j) = log(P(Rx_I_Uniform,ASK(Gray_table(j,:)==0),Standard_variance1_Uniform,ones(1,M_ASK/2)/M_ASK)./...
-                P(Rx_I_Uniform,ASK(Gray_table(j,:)==1),Standard_variance1_Uniform,ones(1,M_ASK/2)/M_ASK));
+            llr_I_Uniform(:,j) = log(P(Rx_I_Uniform,ASK(Gray_table(j,:)==0),Standard_variance1_Uniform,ones(1,M/2)/M)./...
+                P(Rx_I_Uniform,ASK(Gray_table(j,:)==1),Standard_variance1_Uniform,ones(1,M/2)/M));
             
         end
         %随机分布
         %解交织映射
-        llr_I_info_pidemap_Uniform = llr_I_Uniform(:,bit_map_pi_Uniform)';
+        llr_I_info_pidemap_Uniform(bit_map_pi_Uniform,:) = llr_I_Uniform';
         llr_I_ldpc_Uniform = llr_I_info_pidemap_Uniform(:);
         %译码
         rxBits_I_Uniform = ldpcDecoder(llr_I_ldpc_Uniform);
