dc557bd6fc70c2c9956db3f7220eabbf78194cd5
JianHong <792585905@qq.com>
2022-02-28 17:06:02 +0800

    解决了一个会产生inf的问题

--- code/Compute_fun/mutualinfo.m
+++ code/Compute_fun/mutualinfo.m
@@ -3,14 +3,17 @@ function [I] = mutualinfo(px,delta,X)
 %   此处显示详细说明
 I=0;
 
-for i = 1:length(X)
-    
     p_y_x = @(x,u) (1/sqrt(2*pi)*exp(-(x-u).^2./2));
     p_y = @(x) p_y_x(x,delta*X)*(px');
+    
+for i = 1:length(X)
+    
 
-    p_yxi =@(x) min(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0) ...
-              + max(p_y_x(x,delta*X(i)) * px(i) .* log2(p_y_x(x,delta*X(i))./p_y(x)),0);
+
+    p_yxi =@(x) min(p_y_x(x,delta*X(i)) * px(i) .* (log2(p_y_x(x,delta*X(i)))-log2(p_y(x))),0) ...
+              + max(p_y_x(x,delta*X(i)) * px(i) .* (log2(p_y_x(x,delta*X(i)))-log2(p_y(x))),0);
 %min() + max() 的原因是当值过小时会产生NaN,该方法可以去掉NaN的同时保留正负值
+%对数拆开计算可以避免一些inf值的产生
 
 %     I = I + integral(p_yxi,-inf,inf,'ArrayValued',true);
     I = I + integral(p_yxi,delta*X(i)-abs(10*delta*X(i)),delta*X(i)+abs(10*delta*X(i)),'ArrayValued',true);
